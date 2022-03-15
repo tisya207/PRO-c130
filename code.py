1e8976c9262fa39029c147d40b62171ea3d31fa2
tisya207 <65344664+tisya207@users.noreply.github.com>
2022-03-15 13:51:54 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -4,16 +4,13 @@ import csv
 df= pd.read_csv("merged_stars_data_final.csv")
 print(df.shape)
 
+del df["Star_name"]
+del df["Mass"]
+del df["Radius"]
+del df["Distance"]
 del df["Luminosity"]
 
 print(df.shape)
 
-df = df.rename({
-                'Star_name': "star_names", 
-                'Distance': "distance", 
-                'Mass': "mass", 
-                'Radius': "radius", 
-                'Luminosity': "luminosity", 
-            }, axis='columns')
 
-df.to_csv('main2.csv') 
+df.to_csv('main.csv') 
